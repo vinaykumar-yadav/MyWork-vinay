84711ec062072a73e60f93b3f6d9658690e996b5
vinaykumar-yadav <vinaykumar.yadav@hcl.com>
2021-02-23 11:27:44 +0530

    23_feb_2021

--- test5.py
+++ test5.py
@@ -1,2 +1,5 @@
 import os.path, time
 print("Last modified: %s" % time.ctime(os.path.getmtime("test5.py"))) 
+
+print(os.path.join(os.getcwd(), os.pardir))
+print(os.path.abspath(os.path.join(os.getcwd(), os.pardir)))
